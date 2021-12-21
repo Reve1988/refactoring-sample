76cd909b1616ed564fde63f4340cdf4142e73f57
hypering <glesmallow@gmail.com>
2021-12-22 00:44:56 +0900

    refactor : csv 파일 읽어서 특정 컬럼명을 group by 작업 리팩토링

--- src/main/java/kr/revelope/study/refactoring/DirtyCodeMain.java
+++ src/main/java/kr/revelope/study/refactoring/DirtyCodeMain.java
@@ -1,18 +1,21 @@
 package kr.revelope.study.refactoring;
 
-import java.io.BufferedReader;
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * csv 파일을 읽어서 특정 컬럼명을 group by 하여 출력하는 프로그램이다.
  * args[0] : resources에 보관된 csv 파일명
  * args[1] : 카운트 할 컬럼명
- *
+ * <p>
  * 아래 코드를 리팩토링 해보시오
  */
 public class DirtyCodeMain {
@@ -21,54 +24,35 @@ public class DirtyCodeMain {
 			throw new IllegalArgumentException("File name and target column name is required.");
 		}
 
-		InputStream is = DirtyCodeMain.class.getClassLoader().getResourceAsStream(args[0]);
-		if (is == null) {
+		InputStream inputStream = DirtyCodeMain.class.getClassLoader().getResourceAsStream(args[0]);
+		if (inputStream == null) {
 			throw new IllegalArgumentException("'" + args[0] + "' file can not found.");
 		}
 
-		Map<String, Integer> result = new HashMap<>();
-		try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
-			 BufferedReader reader = new BufferedReader(streamReader)) {
-			String line;
-			int columnCount = -1;
-			int targetColumnIndex = -1;
-			boolean isHeader = true;
-			while ((line = reader.readLine()) != null) {
-				if (isHeader) {
-					String[] columns = line.split(",");
-					if (columns.length == 0) {
-						throw new IllegalArgumentException("First line must be columns. Column can not found.");
-					}
+		try (InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+			 CSVReader csvReader = new CSVReader(streamReader)) {
 
-					columnCount = columns.length;
-					for (int i = 0; i < columnCount; i++) {
-						if (columns[i].equals(args[1])) {
-							targetColumnIndex = i;
-						}
-					}
+			String[] header = csvReader.readNext();
+			if (header == null || header.length == 0) {
+				throw new IllegalArgumentException("First line must be columns. Column can not found.");
+			}
 
-					if (targetColumnIndex < 0) {
-						throw new IllegalStateException("Can not found target column '" + args[1] + "'");
-					}
+			int columnIndex = ArrayUtils.indexOf(header, args[1]);
+			if (columnIndex == -1) {
+				throw new IllegalArgumentException("First line must be columns. Column can not found.");
+			}
 
-					isHeader = false;
-					continue;
-				}
+			List<String[]> dataList = csvReader.readAll();
+			Map<String, List<String>> result = dataList.stream()
+					.filter(data -> data.length == header.length)
+					.map(data -> data[columnIndex])
+					.collect(Collectors.groupingBy(column -> column));
 
-				String[] columns = line.split(",");
-				if (columns.length != columnCount) {
-					System.out.println("Column count is not matched. must be " + columnCount + ", but " + columns.length);
-					continue;
-				}
-
-				result.put(columns[targetColumnIndex], result.get(columns[targetColumnIndex]) != null ? result.get(columns[targetColumnIndex]) + 1 : 1);
+			for (String column : result.keySet()) {
+				System.out.println(column + " : " + result.get(column).size());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-
-		for (Map.Entry<String, Integer> entry : result.entrySet()) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
 		}
 	}
 }
